aaa3dc028132eb6ce9737a3983c3d2992b3e0c1e
ratoleproso <annecaroly.78@gmail.com>
2026-08-18 16:14:44 -0300

    api

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ app.use(cors({
     origin: ['http://localhost:5500', 'http://127.0.0.1:5500']
 }));
 
+const alunos = [
+    { "nome": "Maria", "idade": 30 },
+    { "nome": "João", "idade": 25 },
+    { "nome": "Pedro", "idade": 20 }
+]
 
 app.get('/alunos', (req, res) => {
- res.send("lendo dados do banco...")
+ res.send(alunos)
 })
 
 app.listen(3000, () => {
@@ -18,7 +23,11 @@ app.listen(3000, () => {
 });
 
 app.post("/alunos", (req, res) => {
+    
     const {nome, url} = req.body
-    res.json({"nome": nome, "url": url})
+
+    console.log(`Nome digitado: ${nome} URL: ${url}`)
+
+    //res.json({"nome": nome, "url": url})
 })
 
